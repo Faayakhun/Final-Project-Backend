643f0a90df18d7d8f60531d84da6b3903e38853f
Faayakhun <faayakhun@gmail.com>
2021-04-07 16:39:12 +0700

    first push

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const {PORT, dbConfigMongo} = require('./config')
+
+const verifyToken = require('./middleware/authorization')
+
+const localPort =PORT || 3000
+console.log('running on',localPort)
+app.use(cors())
+app.use(express.json())
+
+app.get("/", (req,res)=> {
+    res.send("<h1 style='text-align:center'>Welcome to my database</h1>"+"<h3 style='text-align:center'>Backend for final project</h3>"+"<h3 style='text-align:center'>Team 1 Fighting!!</h3>")
+})
+
+
+
+
+
+if (dbConfigMongo) {
+    app.listen(PORT, ()=> {
+        console.log("server running on port",PORT)
+        console.log("connected to mongoDB")
+    })
+} else {
+    console.log("not connect to mongo")
+}
