903095acbcd869c64a3f4aa34345c82b15da6605
suryanto23 <sur23.id@gmail.com>
2021-04-07 20:40:16 +0700

    suryanto-membuat model vendor

---
+++ models/vendor.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const VendorSchema = new mongoose.Schema({
+    namaVendor: {
+        type: String,
+        require: true
+    },
+    passwordVendor: {
+        type: String,
+        require: true
+    },
+    nomorTelepon: {
+        type: String,
+        require: true
+    },
+    alamatKantor: {
+        type: String,
+        require: true
+    },
+    logoKantor : String,
+    portfolio : [{
+        type: String
+    }],
+    review: String
+
+});
+
+const Vendor = mongoose.model("vendor" , VendorSchema);
+
+module.exports=Vendor;
