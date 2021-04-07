0dd4ec7a3b5f66ac5d443711affa1aa6eb339b18
Faayakhun <faayakhun@gmail.com>
2021-04-07 20:01:01 +0700

    add find by id

--- routes/MandorController.js
+++ routes/MandorController.js
@@ -16,4 +16,17 @@ router.get('/', async (req,res)=> {
     }
 })
 
+router.get('/:id', async (req,res)=> {
+    const mandor = await Mandor.findById(req.params.id)
+    try {
+        res.status(200).json({
+            message: 'success get mandor data by id',
+            data: mandor
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).send(error)
+    }
+})
+
 module.exports = router
