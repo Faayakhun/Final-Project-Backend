10fc8ba507bf91466e0999c942b1981123d7491b
Faayakhun <faayakhun@gmail.com>
2021-04-23 14:36:03 +0700

    add put mandor

--- routes/MandorController.js
+++ routes/MandorController.js
@@ -29,4 +29,17 @@ router.get('/:id', async (req,res)=> {
     }
 })
 
+router.put('/:id',async (req,res) => {
+    const mandorUpdate = req.body
+    const mandor = await Mandor.findByIdAndUpdate(req.params.id,mandorUpdate)
+    try {
+        res.status(200).json({
+            message: 'success update mandor data by id',
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).send(error)
+    }
+})
+
 module.exports = router
